4921e44eb200a25f8c8844fb6eb119e6450bc4f3
sonia <amirisomayeh74@gmail.com>
2019-04-06 12:17:44 -0700

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 "use strict";
+
+
+
+let date = new Date(2012, 1, 20, 3, 12);
+console.log( date );
+
+
+
+
+
+function getSecondsToday() {
+    let now = new Date();
+    return now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+  }
+
+ console.log(getSecondsToday()); 
+  
+
+
+
+
+ 
+function getSecondsToTomorrow() {
+
+    let now1 = new Date();
+    let hour = now1.getHours();
+    let minutes = now1.getMinutes();
+    let seconds = now1.getSeconds();
+    let sumSecondsToday = (hour * 60 + minutes) * 60 + seconds;
+    let sumSecondsInADay = 86400;
+  
+    return sumSecondsInADay - sumSecondsToday;
+  }
+  
+console.log(getSecondsToTomorrow());
+
+
+
+
+
+let user = {
+    name: "John Smith",
+    age: 35
+  };
+  
+ let user1 = JSON.parse(JSON.stringify(user));
+  
--- test/test.js
+++ test/test.js
@@ -1,42 +1,96 @@
 let assert = require('chai').assert;
 
-function inArray(arr) {
-  return function(x) {
-    return arr.includes(x);
-  };
+
+
+
+function getWeekDay(date) {
+  let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+
+  return days[date.getDay()];
 }
 
+let date = new Date(2012, 0, 3);
+console.log( getWeekDay(date) ); 
 
 
-function inBetween(a, b) {
-  return function(x) {
-    return x >= a && x <= b;
-  };
-}
-
-
-
-
-
-describe("inArray", function() {
-  let arr = [1, 2, 3, 4, 5, 6, 7];
-
-  it("returns the filter for values in array", function() {
-
-    let filter = inArray([1,2,10]);
-    assert.isTrue(filter(2));
-    assert.isFalse(filter(0));
+describe("getWeekDay",function(){
+  it("3 january 2012-tuesday",function(){
+    let date = new Date(2012,0,3);
+    assert.equal(getWeekDay(new Date(2012,0,3)),'TU');
+  });
+  it("5 january 2012-thursday",function(){
+    date = new Date(2012-0-5);
+    assert.equal(getWeekDay(new Date(2012,0,5)),'TH');
   });
 });
 
 
-describe("inBetween", function() {
 
-  it("returns the filter for values between", function() {
-    let filter = inBetween(3, 6);
-    assert.isTrue(filter(5));
-    assert.isFalse(filter(0));
+
+
+function getLocalDay(date2) {
+
+  let day = date2.getDay();
+
+  return day;
+}
+
+let date2 = new Date(2012,0,3);
+console.log(getLocalDay(date2));
+
+
+describe("getLocalDay returns the iranian  weekday",function(){
+  it("3 january 2012-tuesday",function(){
+    date2 = new Date(2012,0,3);
+    assert.equal(getLocalDay(new Date(2012,0,3)),4);
+  });
+  it("5 january 2012-friday",function(){
+    date2 = new Date(2012-0-6);
+    assert.equal(getLocalDay(new Date(2012,0,6)),7);
   });
 });
 
 
+
+
+
+function getDateAgo(date3, days) {
+  const date = new Date(date3);
+
+  date.setDate(date3.getDate() - days);
+  return date.getDate();
+}
+
+let date3 = new Date(2015, 0, 2);
+
+
+describe("getDateAgo",function(){
+  it("1 day befor date",function(){
+    console.log( getDateAgo(date3, 1) );
+    assert.equal(getDateAgo(new Date(2015,0,2),1),1);
+  });
+  it("2 days befor date",function(){
+    console.log( getDateAgo(date3, 2));
+    assert.equal(getDateAgo(new Date(2015,0,2),2),31);
+  });
+  it("365 days befor date",function(){
+    console.log( getDateAgo(date3, 365) );
+    assert.equal(getDateAgo(new Date(2015,0,2),365),2);
+  });
+});
+
+
+
+
+
+function getLastDayOfMonth(year, month) {
+  let date4 = new Date(year, month + 1);
+  return date4.getDate();
+}
+
+describe("getLastDayOfMonth",function(){
+  it("last day of 01.01.2012",function(){
+    console.log( getLastDayOfMonth(2012, 0));
+    assert.equal(getLastDayOfMonth(2012,0),31);
+  });
+});
