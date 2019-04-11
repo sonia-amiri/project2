1598ae8cc388b49cd1cc32812dcb571c6b4a3a1f
Hamed Qaderi <hamedghaderii@gmail.com>
2019-04-11 13:26:41 +0430

    Tanx

--- app.js
+++ app.js
@@ -1,48 +1,28 @@
-"use strict";
-
-
-
-let date = new Date(2012, 1, 20, 3, 12);
-console.log( date );
-
-
-
-
+'use strict';
 
 function getSecondsToday() {
-    let now = new Date();
-    return now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
-  }
+  let now = new Date();
+  return now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+}
 
- console.log( getSecondsToday() ); 
-  
+console.log(getSecondsToday());
 
-
-
-
- 
 function getSecondsToTomorrow() {
+  let now1 = new Date();
+  let hour = now1.getHours();
+  let minutes = now1.getMinutes();
+  let seconds = now1.getSeconds();
+  let sumSecondsToday = (hour * 60 + minutes) * 60 + seconds;
+  let sumSecondsInADay = 86400;
 
-     let now1 = new Date();
-     let hour = now1.getHours();
-     let minutes = now1.getMinutes();
-     let seconds = now1.getSeconds();
-     let sumSecondsToday = (hour * 60 + minutes) * 60 + seconds;
-     let sumSecondsInADay = 86400;
-  
-    return sumSecondsInADay - sumSecondsToday;
-  }
-  
-console.log( getSecondsToTomorrow() );
+  return sumSecondsInADay - sumSecondsToday;
+}
 
-
-
-
+console.log(getSecondsToTomorrow());
 
 let user = {
-    name: "John Smith",
-    age: 35
-  };
+  name: 'John Smith',
+  age: 35
+};
 
-let user1 = JSON.parse( JSON.stringify(user) );
-  
+let user1 = JSON.parse(JSON.stringify(user));
--- test/test.js
+++ test/test.js
@@ -1,73 +1,40 @@
 let assert = require('chai').assert;
 
-
-
-
 function getWeekDay(date) {
-
    let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
 
-   return days[ date.getDay() ];
+   return days[date.getDay()];
 }
 
-let date = new Date(2012, 0, 3);
+describe('getWeekDay', function () {
+   it('3 january 2012-tuesday', function () {
+      let date = new Date(2012, 0, 3);
+      assert.equal(getWeekDay(new Date(2012, 0, 3)), 'TU');
+   });
 
-console.log( getWeekDay(date) ); 
-
-
-describe("getWeekDay", function(){
-
-   it("3 january 2012-tuesday", function(){
-
-      let date = new Date(2012,0,3);
-      assert.equal( getWeekDay( new Date(2012, 0, 3)), 'TU');
-  });
-
-  it("5 january 2012-thursday", function(){
-
-      date = new Date(2012-0-5);
-      assert.equal( getWeekDay( new Date(2012, 0, 5)), 'TH');
-  });
-
+   it('5 january 2012-thursday', function () {
+      date = new Date(2012 - 0 - 5);
+      assert.equal(getWeekDay(new Date(2012, 0, 5)), 'TH');
+   });
 });
-
-
-
-
 
 function getLocalDay(date2) {
+   let day = date2.getDay();
 
-  let day = date2.getDay();
-
-  return day;
+   return day;
 }
 
-let date2 = new Date(2012, 0, 3);
+describe('getLocalDay returns the iranian  weekday', function () {
+   it('2 january 2012-tuesday', function () {
+      date2 = new Date(2012, 0, 3);
+      assert.equal(getLocalDay(new Date(2012, 0, 3)), 2);
+   });
 
-console.log( getLocalDay(date2) );
-
-
-describe("getLocalDay returns the iranian  weekday", function(){
-
-  it("3 january 2012-tuesday", function(){
-
-     date2 = new Date(2012, 0, 3);
-     assert.equal( getLocalDay( new Date(2012, 0, 3)), 4);
-
-  });
-
-  it("5 january 2012-friday", function(){
-
-     date2 = new Date(2012, 0, 6);
-     assert.equal( getLocalDay( new Date(2012, 0, 6)), 7);
-
-  });
-
+   it('5 january 2012-friday', function () {
+      date2 = new Date(2012, 0, 6);
+      assert.equal(getLocalDay(new Date(2012, 0, 6)), 5);
+   });
 });
-
-
-
-
 
 function getDateAgo(date3, days) {
    const date = new Date(date3);
@@ -79,32 +46,22 @@ function getDateAgo(date3, days) {
 
 let date3 = new Date(2015, 0, 2);
 
+describe('getDateAgo', function () {
+   it('1 day befor date', function () {
+      console.log(getDateAgo(date3, 1));
+      assert.equal(getDateAgo(new Date(2015, 0, 2), 1), 1);
+   });
 
-describe("getDateAgo", function(){
+   it('2 days befor date', function () {
+      console.log(getDateAgo(date3, 2));
+      assert.equal(getDateAgo(new Date(2015, 0, 2), 2), 31);
+   });
 
-   it("1 day befor date", function(){
-
-     console.log( getDateAgo(date3, 1) );
-     assert.equal( getDateAgo( new Date(2015, 0, 2), 1), 1);
-  });
-
-   it("2 days befor date", function(){
-
-     console.log( getDateAgo(date3, 2));
-     assert.equal( getDateAgo( new Date(2015, 0, 2), 2), 31);
-  });
-
-   it("365 days befor date", function(){
-
-     console.log( getDateAgo(date3, 365) );
-     assert.equal( getDateAgo( new Date(2015, 0, 2), 365), 2);
-  });
-
+   it('365 days befor date', function () {
+      console.log(getDateAgo(date3, 365));
+      assert.equal(getDateAgo(new Date(2015, 0, 2), 365), 2);
+   });
 });
-
-
-
-
 
 function getLastDayOfMonth(year, month) {
    let date4 = new Date(year, month + 1);
@@ -112,12 +69,8 @@ function getLastDayOfMonth(year, month) {
    return date4.getDate();
 }
 
-describe("getLastDayOfMonth", function(){
-
-   it("last day of 01.01.2012", function(){
-
-    console.log( getLastDayOfMonth(2012, 0));
-    assert.equal(getLastDayOfMonth(2012, 0), 31);
-  });
-  
+describe('getLastDayOfMonth', function () {
+   it('last day of 01.01.2012', function () {
+      assert.equal(getLastDayOfMonth(2012, 0), 1);
+   });
 });
