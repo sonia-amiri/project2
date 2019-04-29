b6bf3d89b7f25404556af2bf79da0d32d10be2c9
sonia <amirisomayeh74@gmail.com>
2019-04-29 04:55:00 -0700

    recursion project

--- app.js
+++ app.js
@@ -1,28 +1,169 @@
 'use strict';
 
-function getSecondsToday() {
-  let now = new Date();
-  return now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+function sumToF(n){
+  let sum=0;
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+    
+  }
+  return sum;
 }
 
-console.log(getSecondsToday());
 
-function getSecondsToTomorrow() {
-  let now1 = new Date();
-  let hour = now1.getHours();
-  let minutes = now1.getMinutes();
-  let seconds = now1.getSeconds();
-  let sumSecondsToday = (hour * 60 + minutes) * 60 + seconds;
-  let sumSecondsInADay = 86400;
 
-  return sumSecondsInADay - sumSecondsToday;
+function sumToR(n){
+  if(n==1){
+    return 1;
+  }
+  return n + sumToR(n-1);
 }
 
-console.log(getSecondsToTomorrow());
 
-let user = {
-  name: 'John Smith',
-  age: 35
+
+function sumToMath(n){
+  return n*(n+1)/2;
+}
+
+
+function benchMark (fn){
+  let n=100; 
+  for (let i = 0; i < 100000; i++) {
+    return fn(n);
+  }
+}
+
+
+// let sumF=0;
+// let sumR=0;
+// let sumM=0;
+
+
+
+// benchMark(sumToF);
+// benchMark(sumToR);
+// benchMark(sumToMath);
+
+
+// for (let i = 0; i < 10; i++) {
+//  sumF += benchMark(sumToF);
+//  sumR += benchMark(sumToR);
+//  sumM += benchMark(sumToMath);
+// }
+
+
+console.log("forLoop:"+ benchMark(sumToF));
+console.log("recursion:"+ benchMark(sumToR));
+console.log("arithmetic:"+ benchMark(sumToMath));
+
+
+
+function fib(n) {
+  let x = 1;
+  let y = 1;
+  for (let i = 3; i <= n; i++) {
+    let z = x + y;
+    x = y;
+    y = z;
+  }
+  return y;
+}
+
+console.log(fib(3));
+console.log(fib(7));
+console.log(fib(77));
+
+
+
+let list ={
+  value:1,
+  next:{
+    value:2,
+    next:
+    {
+      value:3,
+      next:{
+        value:4,
+        next:null
+      }
+    }
+  }
 };
 
-let user1 = JSON.parse(JSON.stringify(user));
+function PrintListL(list){
+    let i =list;
+    while(i){
+      console.log(i.value);
+      i=i.next;
+    }
+}
+
+PrintListL(list);
+
+
+
+function printList(list) {
+
+  console.log(list.value);
+
+  if (list.next) {
+    printList(list.next); 
+  }
+
+}
+
+printList(list);
+
+function bench(fn1){
+  
+  for (let i = 0; i < 100; i++) {
+    return fn1(list);
+    
+  }
+}
+
+
+// let printL = 0;
+// let printR = 0;
+
+// bench(printL);
+// bench(printList);
+
+// for (let i = 0; i < 10; i++) {
+//   printL += bench(PrintListL);
+//   printR += bench(printList);
+  
+// }
+
+console.log("forLoop:"+bench(PrintListL));
+console.log("recursion:"+bench(printList));
+
+
+
+
+
+// function printReverseList(list){
+//   let arr =[];
+//   let step =list;
+//   while(step){
+//     arr.push(step.value);
+//     step = step.next;
+//   }
+
+//   for (let i=arr.length-1; i>= 0;i++) {
+//     console.log(arr[i]);
+    
+//   }
+// }
+
+// printReverseList(list);
+
+
+
+function printReverseListR(list){
+  if (list.next) {
+    console.log(printReverseListR(list.next));
+  }
+  console.log(list.value);
+}
+
+printReverseListR(list);
--- test/test.js
+++ test/test.js
@@ -1,76 +1,20 @@
 let assert = require('chai').assert;
 
-function getWeekDay(date) {
-   let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
 
-   return days[date.getDay()];
-}
 
-describe('getWeekDay', function () {
-   it('3 january 2012-tuesday', function () {
-      let date = new Date(2012, 0, 3);
-      assert.equal(getWeekDay(new Date(2012, 0, 3)), 'TU');
-   });
 
-   it('5 january 2012-thursday', function () {
-      date = new Date(2012 - 0 - 5);
-      assert.equal(getWeekDay(new Date(2012, 0, 5)), 'TH');
-   });
-});
+function factorial(n){
+    if (n==1) {
+      return 1;
+    } else {
+      return n*factorial(n-1);
+    }
+  }
 
-function getLocalDay(date2) {
-   let day = date2.getDay();
 
-   return day;
-}
-
-describe('getLocalDay returns the iranian  weekday', function () {
-   it('2 january 2012-tuesday', function () {
-      date2 = new Date(2012, 0, 3);
-      assert.equal(getLocalDay(new Date(2012, 0, 3)), 2);
-   });
-
-   it('5 january 2012-friday', function () {
-      date2 = new Date(2012, 0, 6);
-      assert.equal(getLocalDay(new Date(2012, 0, 6)), 5);
-   });
-});
-
-function getDateAgo(date3, days) {
-   const date = new Date(date3);
-
-   date.setDate(date3.getDate() - days);
-
-   return date.getDate();
-}
-
-let date3 = new Date(2015, 0, 2);
-
-describe('getDateAgo', function () {
-   it('1 day befor date', function () {
-      console.log(getDateAgo(date3, 1));
-      assert.equal(getDateAgo(new Date(2015, 0, 2), 1), 1);
-   });
-
-   it('2 days befor date', function () {
-      console.log(getDateAgo(date3, 2));
-      assert.equal(getDateAgo(new Date(2015, 0, 2), 2), 31);
-   });
-
-   it('365 days befor date', function () {
-      console.log(getDateAgo(date3, 365));
-      assert.equal(getDateAgo(new Date(2015, 0, 2), 365), 2);
-   });
-});
-
-function getLastDayOfMonth(year, month) {
-   let date4 = new Date(year, month + 1);
-
-   return date4.getDate();
-}
-
-describe('getLastDayOfMonth', function () {
-   it('last day of 01.01.2012', function () {
-      assert.equal(getLastDayOfMonth(2012, 0), 1);
-   });
+  
+describe("factorial",function(){
+    it('calculate n! using recursion',function(){
+        assert.equal(factorial(5),120);
+    });
 });
